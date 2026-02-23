dfb735da11e30100591c763fd1ca6e39c42f3392
welitaluisa <welita.luisa@gmail.com>
2026-02-23 09:04:08 -0300

    modificando o teste login para usar URL do env

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,7 @@
 describe('Login', () => {
   beforeEach(() => {
      //Arrange
-    cy.visit('http://localhost:4000')
+    cy.visit(Cypress.env('URL'))
     cy.screenshot('apos-visitar-a-pagina')
   })
 
