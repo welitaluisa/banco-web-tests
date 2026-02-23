85d63d25eb7310801f8c481db9dba7d1395c2311
welitaluisa <welita.luisa@gmail.com>
2026-02-23 08:43:18 -0300

    Adicionei arquivo credenciais na pasta fixtures e alteração do gitignore

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,8 +8,11 @@ describe('Login', () => {
   it('Login com dados válidos deve permitir entrada no sistema', () => {
 
     //Act
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('123456')
+    cy.fixture('credenciais').then((credenciais) => {
+      cy.get('#username').click().type(credenciais.valida.usuario)
+      cy.get('#senha').click().type(credenciais.valida.senha)
+    })
+     
     cy.contains('button', 'Entrar').click()
     cy.screenshot('apos-clicar-em-entrar')
 
@@ -22,8 +25,11 @@ describe('Login', () => {
     it('Login com dados inválidos deve exibir mensagem de erro', () => {
   
     //Act
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('789999')
+    cy.fixture('credenciais').then((credenciais) => {
+       cy.get('#username').click().type(credenciais.invalida.usuario)
+       cy.get('#senha').click().type(credenciais.invalida.senha)
+    })
+      
     cy.contains('button', 'Entrar').click()
     
     //Assert
